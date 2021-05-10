ae5a43158fe637db2398b215cc27967317f92970
Hiroyuki Kusu <kusu0806@gmail.com>
2021-05-10 22:59:36 +0900

    Full renewal. Supports private bucket

--- src/main.js
+++ src/main.js
@@ -1,90 +1,158 @@
 const core = require('@actions/core');
 const aws = require('aws-sdk');
-const s3 = new aws.S3();
 const fs = require('fs');
 const path = require('path');
 const qr = require('qrcode');
 
-// If you want to run it locally, prepare a credential file
-// ref: https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/cli-configure-files.html
-
 const NODE_ENV = process.env['NODE_ENV'];
+
+// If you want to run it locally, set the environment variables like `$ export SOME_KEY=<your token>`
+const AWS_ACCESS_KEY_ID = process.env['AWS_ACCESS_KEY_ID'];
+const AWS_SECRET_ACCESS_KEY = process.env['AWS_SECRET_ACCESS_KEY'];
+const AWS_BUCKET = process.env['AWS_BUCKET'];
 
 let input;
 if (NODE_ENV != 'local') {
   input = {
-    s3Bucket: core.getInput('s3-bucket', { required: true }),
-    sourcePath: core.getInput('source-path', { required: true }),
-    destinationPath: core.getInput('destination-path', { required: true }),
-    downloadBaseUrl: core.getInput('download-base-url'),
-    downloadQrPath: core.getInput('download-qr-path'),
-    downloadQrWidth: core.getInput('download-qr-width', { required: true }),
+    awsAccessKeyId: core.getInput('aws-access-key-id', { required: true }),
+    awsSecretAccessKey: core.getInput('aws-secret-access-key', { required: true }),
+    awsRegion: core.getInput('aws-region', { required: true }),
+    awsBucket: core.getInput('aws-bucket', { required: true }),
+    filePath: core.getInput('file-path', { required: true }),
+    contentType: core.getInput('content-type'),
+    destinationDir: core.getInput('destination-dir'),
+    private: core.getInput('private'),
+    outputUrl: core.getInput('output-url'),
+    expire: core.getInput('expire'),
+    createQr: core.getInput('create-qr'),
+    qrWidth: core.getInput('qr-width'),
+    bucketRoot: core.getInput('bucket-root'),
   };
 } else {
   input = {
-    s3Bucket: 'my-bucket',
-    sourcePath: './sample.txt',
-    destinationPath: 'abcdef/sample.txt',
-    downloadBaseUrl: 'http://my-bucket.s3-website-ap-northeast-1.amazonaws.com',
-    downloadQrPath: 'abcdef/qr.png',
-    downloadQrWidth: '120',
+    awsAccessKeyId: AWS_ACCESS_KEY_ID,
+    awsSecretAccessKey: AWS_SECRET_ACCESS_KEY,
+    awsRegion: 'ap-northeast-1',
+    awsBucket: AWS_BUCKET,
+    filePath: './README.md',
+    contentType: '',
+    destinationDir: '',
+    private: 'true',
+    outputUrl: 'false',
+    expire: '180',
+    createQr: 'false',
+    qrWidth: '120',
+    bucketRoot: 'artifacts',
   };
 }
 
+aws.config.update({
+  accessKeyId: input.awsAccessKeyId,
+  secretAccessKey: input.awsSecretAccessKey,
+  region: input.awsRegion,
+});
+
+const s3 = new aws.S3({signatureVersion: 'v4'});
+
 async function run(input) {
 
-  if (path.isAbsolute(input.destinationPath)) {
-    throw new Error('"destinationPath" should be a relative path.');
+  const expire = parseInt(input.expire);
+  if (!expire | expire < 0 | 604800 < expire) {
+    throw new Error('"expire" input should be a number between 0 and 604800.');
   }
 
-  if (input.destinationPath.endsWith('/')) {
-    throw new Error('"destinationPath" should be a file.');
+  const qrWidth = parseInt(input.qrWidth);
+  if (!qrWidth | qrWidth < 100 | 1000 < qrWidth) {
+    throw new Error('"qr-width" input should be a number between 100 and 1000.');
   }
 
-  if (input.downloadBaseUrl) {
-    if (!input.downloadBaseUrl.startsWith('http://') && !input.downloadBaseUrl.startsWith('http://')) {
-      throw new Error('"downloadBaseUrl" should be a URL.');
+  let bucketRoot = input.bucketRoot;
+  if (bucketRoot) {
+    if (bucketRoot.startsWith('/')) {
+      bucketRoot = bucketRoot.slice(1);
     }
-    if (!input.downloadBaseUrl.endsWith('/')) {
-      input.downloadBaseUrl = input.downloadBaseUrl + '/'
-    }
-  }
-
-  if (input.downloadQrPath) {
-    if (path.isAbsolute(input.downloadQrPath)) {
-      throw new Error('"downloadQrPath" should be a relative path.');
-    }
-    if (!input.downloadQrPath.endsWith('.png')) {
-      throw new Error('"downloadQrPath" should be a png file.');
+    if (!bucketRoot.endsWith('/')) {
+      bucketRoot = bucketRoot + '/'
     }
   }
 
-  const downloadQrWidth = parseInt(input.downloadQrWidth);
-
-  if (!downloadQrWidth | downloadQrWidth < 100 | 1000 < downloadQrWidth) {
-    throw new Error('"downloadQrWidth" should be a number between 100 and 1000.');
+  let destinationDir = input.destinationDir;
+  if (destinationDir) {
+    if (destinationDir.startsWith('/')) {
+      destinationDir = destinationDir.slice(1);
+    }
+    if (!destinationDir.endsWith('/')) {
+      destinationDir = destinationDir + '/'
+    }
+  } else {
+    destinationDir = getRandomStr(32) + '/';
   }
 
-  const params = {
-    Bucket: input.s3Bucket,
-    Key: input.destinationPath,
-    Body: fs.readFileSync(input.sourcePath),
+  const fileKey = bucketRoot + destinationDir + path.basename(input.filePath);
+
+  let acl;
+  if (input.private == 'true') {
+    acl = 'private';
+  } else {
+    acl = 'public-read';
+  }
+
+  let params = {
+    Bucket: input.awsBucket,
+    Key: fileKey,
+    ContentType: input.contentType,
+    Body: fs.readFileSync(input.filePath),
+    ACL: acl,
   };
   await s3.putObject(params).promise();
 
-  if (input.downloadBaseUrl) {
-    core.setOutput('download-url', input.downloadBaseUrl + input.destinationPath);
-    if (input.downloadQrPath) {
-      await qr.toFile('./s3-upload-action-tmp.png', input.downloadBaseUrl + input.destinationPath, { width: downloadQrWidth })
-      const params = {
-        Bucket: input.s3Bucket,
-        Key: input.downloadQrPath,
-        Body: fs.readFileSync('./s3-upload-action-tmp.png'),
+  let fileUrl;
+  if (input.outputUrl == 'true' || input.createQr == 'true') {
+    if (input.private == 'true') {
+      params = {
+        Bucket: input.awsBucket,
+        Key: fileKey,
+        Expires: expire,
       };
-      await s3.putObject(params).promise();
-      fs.unlinkSync('./s3-upload-action-tmp.png');
-      core.setOutput('download-qr-url', input.downloadBaseUrl + input.downloadQrPath);
+      fileUrl = await s3.getSignedUrlPromise('getObject', params);
+    } else {
+      fileUrl = `https://${input.awsBucket}.s3-${input.awsRegion}.amazonaws.com/${fileKey}`;
     }
+    if (input.outputUrl == 'true') {
+      core.setOutput('file-url', fileUrl);
+    }
+  }
+
+  if (input.createQr != 'true') return;
+
+  const qrKey = bucketRoot + destinationDir + 'qr.png';
+  const tmpQrFile = './s3-upload-action-qr.png';
+
+  await qr.toFile(tmpQrFile, fileUrl, { width: qrWidth })
+
+  params = {
+    Bucket: input.awsBucket,
+    Key: qrKey,
+    ContentType: 'image/png', // Required to display as an image in the browser
+    Body: fs.readFileSync(tmpQrFile),
+    ACL: acl,
+  };
+  await s3.putObject(params).promise();
+  fs.unlinkSync(tmpQrFile);
+
+  if (input.outputUrl == 'true') {
+    let qrUrl;
+    if (input.private == 'true') {
+      params = {
+        Bucket: input.awsBucket,
+        Key: qrKey,
+        Expires: expire,
+      };
+      qrUrl = await s3.getSignedUrlPromise('getObject', params);
+    } else {
+      qrUrl = `https://${input.awsBucket}.s3-${input.awsRegion}.amazonaws.com/${qrKey}`;
+    }
+    core.setOutput('qr-url', fileUrl);
   }
 }
 
@@ -96,3 +164,12 @@ run(input)
     core.setOutput('result', 'failure');
     core.setFailed(error.message);
   });
+
+function getRandomStr(length) {
+  const c = 'abcdefghijklmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789';
+  let r = '';
+  for(let i = 0; i < length; i++) {
+    r += c[Math.floor(Math.random() * c.length)];
+  }
+  return r;
+}

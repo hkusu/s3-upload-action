1ac6a8f09672dc32fe5ce71244620d94137e9913
Hiroyuki Kusu <kusu0806@gmail.com>
2023-09-28 10:59:26 +0900

    Fix bug

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ const s3 = new aws.S3({signatureVersion: 'v4'});
 async function run(input) {
 
   const expire = parseInt(input.expire);
-  if (!expire | expire < 0 | 604800 < expire) {
+  if (isNaN(expire) | expire < 0 | 604800 < expire) {
     throw new Error('"expire" input should be a number between 0 and 604800.');
   }
 
